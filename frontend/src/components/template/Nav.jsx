423aeae5d3a7dec49e72e7ffa752700626922c03
Eduardo Santos Luz <eduardo.luz@compasso.com.br>
2023-11-01 16:58:44 -0400

    feat(Frontend): Adjust Nav component

--- frontend/src/components/template/Nav.jsx
+++ frontend/src/components/template/Nav.jsx
@@ -1,6 +1,18 @@
 import './Nav.css';
 import React from 'react';
 
-const Nav = props => <aside className="menu-area">Navegação</aside>;
+const Nav = props => (
+  <aside className="menu-area">
+    <nav className="menu">
+      {/* Refatorar em casa! */}
+      <a href="#/">
+        <i className="fa fa-home"></i> Início
+      </a>
+      <a href="#/users">
+        <i className="fa fa-users"></i> Usuários
+      </a>
+    </nav>
+  </aside>
+);
 
 export default Nav;
