0717c48174b117e3d1263c0a2b86ae2aec9c3dea
Eduardo Santos Luz <eduardo.luz@compasso.com.br>
2023-11-01 15:59:58 -0400

    feat(Frontend): Adjust Logo component

--- frontend/src/components/template/Logo.jsx
+++ frontend/src/components/template/Logo.jsx
@@ -1,6 +1,13 @@
 import './Logo.css';
+import logoImg from '../../assets/imgs/logo.png';
 import React from 'react';
 
-const Logo = props => <aside className="logo">Logo</aside>;
+const Logo = props => (
+  <aside className="logo">
+    <a href="/" className="logo">
+      <img src={logoImg} alt="logo" />
+    </a>
+  </aside>
+);
 
 export default Logo;
